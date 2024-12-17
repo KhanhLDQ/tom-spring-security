413bd8bf7c7f693f9aa7e0f59c0f3b716fc011cc
Tom <tommapcoder@gmail.com>
2024-12-17 23:45:16 +0700

    spring auth integration & demo all grant type flows

--- src/main/java/org/tommap/springsecurityfirstsection/config/KeycloakOpaqueRoleConverter.java
+++ src/main/java/org/tommap/springsecurityfirstsection/config/KeycloakOpaqueRoleConverter.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenAuthenticationConverter;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -16,13 +17,26 @@ public class KeycloakOpaqueRoleConverter implements OpaqueTokenAuthenticationCon
 
     @Override
     public Authentication convert(String introspectedToken, OAuth2AuthenticatedPrincipal authenticatedPrincipal) {
-        String username = authenticatedPrincipal.getAttribute("preferred_username");
-        Map<String, Object> realmAccess = authenticatedPrincipal.getAttribute("realm_access");
-        Collection<GrantedAuthority> roles = ((List<String>) realmAccess.get("roles")).stream()
+//        String username = authenticatedPrincipal.getAttribute("preferred_username");
+//        Map<String, Object> realmAccess = authenticatedPrincipal.getAttribute("realm_access");
+//        Collection<GrantedAuthority> roles = ((List<String>) realmAccess.get("roles")).stream()
+//                .map(role -> "ROLE_" + role)
+//                .map(SimpleGrantedAuthority::new)
+//                .collect(Collectors.toList());
+//
+//        return new UsernamePasswordAuthenticationToken(username, null, roles);
+
+        List<String> roles = authenticatedPrincipal.getAttribute("scope");
+
+        if (roles == null || roles.isEmpty()) {
+            return new UsernamePasswordAuthenticationToken(authenticatedPrincipal, null, new ArrayList<>());
+        }
+
+        Collection<GrantedAuthority> grantedAuthorities = roles.stream()
                 .map(role -> "ROLE_" + role)
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
 
-        return new UsernamePasswordAuthenticationToken(username, null, roles);
+        return new UsernamePasswordAuthenticationToken(authenticatedPrincipal, null, grantedAuthorities);
     }
 }
--- src/main/java/org/tommap/springsecurityfirstsection/config/KeycloakRoleConverter.java
+++ src/main/java/org/tommap/springsecurityfirstsection/config/KeycloakRoleConverter.java
@@ -8,7 +8,6 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 /*
@@ -19,14 +18,25 @@ import java.util.stream.Collectors;
 public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
     @Override
     public Collection<GrantedAuthority> convert(Jwt source) {
-        Map<String, Object> realmAccess = (Map<String, Object>) source.getClaims().get("realm_access");
+//        Map<String, Object> realmAccess = (Map<String, Object>) source.getClaims().get("realm_access"); //work for Keycloak
+//
+//        if (null == realmAccess || realmAccess.isEmpty()) {
+//            return new ArrayList<>();
+//        }
+//
+//        return ((List<String>) realmAccess.get("roles")).stream()
+//                .map(role -> "ROLE_" + role)
+//                .map(SimpleGrantedAuthority::new)
+//                .collect(Collectors.toList());
 
-        if (null == realmAccess || realmAccess.isEmpty()) {
+        List<String> roles = (ArrayList<String>) source.getClaims().get("roles");
+
+        if (null == roles || roles.isEmpty()) {
             return new ArrayList<>();
         }
 
-        return ((List<String>) realmAccess.get("roles")).stream()
-                .map(role -> "ROLE_" + role)
+        return roles.stream()
+                .map(roleName -> "ROLE_" + roleName)
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
     }
